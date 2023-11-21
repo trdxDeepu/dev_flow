2c5a1fbff0db0b56104cbe0318dff2dcde4ef158
trdxlucifer <singhdepu566@gmail.com>
2023-11-21 22:00:54 +0530

    created a route groups in nextjs

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home () {
-  return (
-    <div>
-      <h1 className='h1-bold'>Hello to next js</h1>
-      <h1 className='h2-bold'>Hello to next js</h1>
-      <h1 className='h3-bold'>Hello to next js</h1>
-    </div>
-  )
-}
